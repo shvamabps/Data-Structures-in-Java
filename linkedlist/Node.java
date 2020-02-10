20d56545266fa07298306e73b500b5bcd1ca5359
shvamabps <37547983+shvamabps@users.noreply.github.com>
2020-02-10 11:51:39 +0530

    Linked List

---
+++ linkedlist/Node.java
@@ -0,0 +1,10 @@
+package linkedlist;
+
+/*
+ * Creating a linked list adding these elements 5,12,6,9,8
+ */
+
+public class Node {
+    int data;
+    Node next;
+}
---
+++ linkedlist/Runner.java
@@ -0,0 +1,15 @@
+package linkedlist;
+
+public class Runner {
+    public static void main(String[] args) {
+        LinkedList list = new LinkedList();
+        list.insert(18);
+        list.insert(45);
+        list.insert(12);
+        list.insertAtStart(25);
+        list.insertAt(2, 55);
+        list.insertAt(0, 36);
+
+        list.show();
+    }
+}
