cf35588a56dafba8569722c444648105118e7825
shvamabps <37547983+shvamabps@users.noreply.github.com>
2020-02-16 16:25:49 +0530

    Sorting

--- sorting/BubbleSortMain.java
+++ sorting/BubbleSortMain.java
@@ -1,20 +1,28 @@
 package sorting;
 
-public class BubbleSortMain {
-	public static void main(String[] args) {
-		int[] a={36,19,29,12,5};
-		for(int i=0;i<a.length;i++){
-			int tmp;
-			for (int j=0;j<a.length ; j++) {
-				if(a[j]>a[j+1]){
-					a[j] = tmp;
-					a[j] = a[j+1];
-					a[j+1] = tmp;
-				}
-			}
-		}
-		for (int i=0; i<a.length; i++) {
-			System.out.primtln(a[i]+"");
-		}
-	}
-} 
+public class BubbleSortMain
+{
+    public static void main(String[] args)
+    {
+        int[] a = {36, 19, 29, 12, 5};
+        for(int i = 0; i < a.length; i++)
+        {
+            int tmp=0;
+            for (int j = 0; j < a.length ; j++)
+            {
+                if(a[j] > a[j + 1])
+                {
+                    a[j] = tmp;
+                    a[j] = a[j + 1];
+                    a[j + 1] = tmp;
+                }
+            }
+        }
+        for (int i = 0; i < a.length; i++)
+        {
+            System.out.println(a[i] + "");
+        }
+    }
+}
+
+
