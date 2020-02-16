a162934658119b0b97f81cbf322f73bcd7171588
shvamabps <37547983+shvamabps@users.noreply.github.com>
2020-02-16 23:24:37 +0530

    Sorting

--- sorting/BubbleSortMain.java
+++ sorting/BubbleSortMain.java
@@ -4,6 +4,7 @@ public class BubbleSortMain {
     static void bubbleSort(int[] arr) {
         int n = arr.length;
         int temp = 0;
+        int flag = 0;
         for (int i = 0; i < n; i++) {
             for (int j = 1; j < (n - i); j++) {
                 if (arr[j - 1] > arr[j]) {
@@ -11,8 +12,11 @@ public class BubbleSortMain {
                     temp = arr[j - 1];
                     arr[j - 1] = arr[j];
                     arr[j] = temp;
+                    flag = 1;
                 }
-
+            }
+            if (flag == 0) {
+                break;
             }
         }
 
